fdc6413876181ce7530c678f2ef0cfc83d82f212
Vinicius Dias <carlosv775@gmail.com>
2019-06-29 15:38:13 -0300

    Adicionando senha no zip
    Video 5.2

--- leitor-zip.php
+++ leitor-zip.php
@@ -1,3 +1,12 @@
 <?php
 
-echo file_get_contents('zip://arquivos.zip#lista-cursos.txt');
+$contexto = stream_context_create([
+    'zip' => [
+        'password' => '123456'
+    ]
+]);
+echo file_get_contents(
+    'zip://arquivos.zip#lista-cursos.txt',
+    false,
+    $contexto
+);
