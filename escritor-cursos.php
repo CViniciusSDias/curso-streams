772036d65d6d9ff5c8b828a9782ffef98a978fc6
Vinicius Dias <carlosv775@gmail.com>
2019-06-29 11:59:09 -0300

    Exlicando modos de abertura. Adicionando conteúdo ao arquivo
    Video 2.2

--- escritor-cursos.php
+++ escritor-cursos.php
@@ -1,8 +1,8 @@
 <?php
 
-$arquivo = fopen('cursos-php.txt', 'w');
+$arquivo = fopen('cursos-php.txt', 'a+');
 
-$curso = 'Design Patterns PHP I: Boas práticas de programação';
+$curso = "\nDesign Patterns PHP II: Boas práticas de programação";
 
 fwrite($arquivo, $curso);
 
