7878cd27cb0c7472c9eddd36fda71c4339986690
Vinicius Dias <carlosv775@gmail.com>
2019-06-29 13:09:57 -0300

    Utilizando file_put_contents
    Video 2.3

--- escritor-cursos.php
+++ escritor-cursos.php
@@ -1,9 +1,5 @@
 <?php
 
-$arquivo = fopen('cursos-php.txt', 'a+');
-
 $curso = "\nDesign Patterns PHP II: Boas práticas de programação";
 
-fwrite($arquivo, $curso);
-
-fclose($arquivo);
+file_put_contents('cursos-php.txt', $curso, FILE_APPEND);
