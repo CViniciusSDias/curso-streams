50c82c38265c6a5f86604da1fdd867898f53a871
Vinicius Dias <carlosv775@gmail.com>
2019-06-29 16:48:59 -0300

    Salvando cursos.csv em ISO-8859-1
    Video 7.2

--- arquivos-para-csv.php
+++ arquivos-para-csv.php
@@ -6,13 +6,13 @@ $outrosCursos = file('cursos-php.txt');
 $arquivoCsv = fopen('cursos.csv', 'w');
 
 foreach ($meusCursos as $curso) {
-    $linha = [trim($curso), 'Sim'];
+    $linha = [trim(utf8_decode($curso)), 'Sim'];
 
     fputcsv($arquivoCsv, $linha, ';');
 }
 
 foreach ($outrosCursos as $curso) {
-    $linha = [trim($curso), 'Não'];
+    $linha = [trim(utf8_decode($curso)), 'Não'];
 
     fputcsv($arquivoCsv, $linha, ';');
 }
--- spl.php
+++ spl.php
@@ -5,7 +5,7 @@ $arquivoCursos = new SplFileObject('cursos.csv');
 while (!$arquivoCursos->eof()) {
     $linha = $arquivoCursos->fgetcsv(';');
 
-    echo $linha[0] . PHP_EOL;
+    echo utf8_encode($linha[0]) . PHP_EOL;
 }
 
 $date = new DateTime();
