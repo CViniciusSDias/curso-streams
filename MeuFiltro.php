bbaaccf0f488392b6ba82d6e01714f5652b5495d
Vinicius Dias <carlosv775@gmail.com>
2019-07-07 05:19:01 -0300

    Verificando se os dados vindo do stream estão separados corretamente por linha antes de filtrar

--- MeuFiltro.php
+++ MeuFiltro.php
@@ -2,19 +2,30 @@
 
 class MeuFiltro extends php_user_filter
 {
-    public $stream;
-
-    public function onCreate()
-    {
-        $this->stream = fopen('php://temp', 'w+');
-        return $this->stream !== false;
-    }
+    private $previousData;
 
     public function filter($in, $out, &$consumed, $closing)
     {
         $saida = '';
+
         while ($bucket = stream_bucket_make_writeable($in)) {
-            $linhas = explode("\n", $bucket->data);
+            if ($closing && !$bucket->datalen) {
+                return PSFS_FEED_ME;
+            }
+            $consumed += $bucket->datalen;
+
+            $stringFromBucket = $bucket->data;
+            if (!empty($this->previousData)) {
+                $stringFromBucket = $this->previousData . $bucket->data;
+                $this->previousData = null;
+            }
+
+            if ($stringFromBucket[-1] !== "\n") {
+                $this->previousData = $stringFromBucket;
+                return PSFS_FEED_ME;
+            }
+
+            $linhas = explode("\n", $stringFromBucket);
 
             foreach ($linhas as $linha) {
                 if (stripos($linha, 'parte') !== false) {
